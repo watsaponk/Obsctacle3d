7a07c63e384bb4fe3a6cbca2a24f4b386bc8651e
watsaponk <k.watsapon@gmail.com>
2021-09-01 09:12:36 +0700

    feat: make player move by keyboard

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [Range(1f, 5f)] public float speed = 1f;
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * 0.01f);
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        
+        transform.Translate(xValue, 0, zValue);
     }
 }
