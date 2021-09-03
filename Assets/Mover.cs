0f12fec72eda398ec2d4fd1f50ee45d653f14103
Watsapon Kampong <k.watsapon@gmail.com>
2021-09-03 08:47:21 +0700

    feat: add wall on field and add rigibody to player

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [Range(5f, 15f)] public float speed = 8f;
+
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        
+
         transform.Translate(xValue, 0, zValue);
     }
+    
 }
